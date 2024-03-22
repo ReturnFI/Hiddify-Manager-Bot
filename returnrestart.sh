2772cea0fee047eb66001a5aa7601455565ede0b
Return <151555003+H-Return@users.noreply.github.com>
2024-03-22 14:33:27 +0330

    Delete returnrestart.sh

--- returnrestart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-HOME=/
-pkill -f telegram_bot.py 
-cd /telegram_bot
-source returnbot/bin/activate
-chmod +x telegram_bot.py
-nohup python3 telegram_bot.py & disown
-clear && echo -e "\n\nBot is restarted!\n\n" && sleep 1
